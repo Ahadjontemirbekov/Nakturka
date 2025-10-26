59b2867e16d8183f3aac189f16f4a9f7cfd7a371
ahadjondasturchi <adhamjontemirbekov054@gmail.com>
2025-10-26 16:59:01 +0500

    pul otkazmalar botim

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import home
 
 urlpatterns = [
-    path('Instagram/Nakrutka/<str:id>/', home, name='home'),
+    path('Nakrutka/<str:id>/', home, name='home'),
 ]
